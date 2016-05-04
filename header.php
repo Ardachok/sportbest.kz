7926d6fc35339b04cfb03ca56bcae7a93df43a70
Ardachok <samatardabek@gmail.com>
2016-05-05 01:49:50 +0600

    Added files via upload

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php
+
+
+ ?>
+
+  <div class="container">
+            <div class="col-md-6 offer" data-animate="fadeInDown">
+                 </div>
+            <div class="col-md-6" data-animate="fadeInDown">
+                <ul class="menu">
+                    <li><a href="controller/logout.php" >Logout</a>
+                    </li>
+                    <li><a href="contact.html">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+       
